8bb5866a4cf0f43a33460489d8d55ddf4f638240
Lucas Borio <lucasborio@gmail.com>
2025-05-20 17:02:54 +0200

    fix priority

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -26,7 +26,7 @@
       >
       <select
         id="taskPriority"
-        v_model="taskPriority"
+        v-model="taskPriority"
         class="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:ring-primary focus:border-primary dark:bg-gray-700 dark:text-gray-200"
       >
         <option value="Low">Low</option>
